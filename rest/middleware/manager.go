a9290382ad5b0cda4ca11caadc94c07c6a908b2a
Md Sowrov <antukhadem17@gmail.com>
2025-12-20 19:49:04 +0600

    initial commit

---
+++ rest/middleware/manager.go
@@ -0,0 +1,39 @@
+package middleware
+
+import "net/http"
+
+type Middleware func(http.Handler) http.Handler
+
+type Manager struct {
+	globalMiddlewares []Middleware
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		globalMiddlewares: make([]Middleware, 0),
+	}
+}
+
+func (mngr *Manager) Use(middlewares ...Middleware) {
+	mngr.globalMiddlewares = append(mngr.globalMiddlewares, middlewares...)
+}
+
+func (mngr *Manager) With(next http.Handler, middlewares ...Middleware) http.Handler {
+	n := next
+
+	for _, middleware := range middlewares {
+		n = middleware(n)
+	}
+
+	return n
+}
+
+func (mngr *Manager) WrapWithMux(next http.Handler, middlewares ...Middleware) http.Handler {
+	n := next
+
+	for _, middleware := range mngr.globalMiddlewares {
+		n = middleware(n)
+	}
+
+	return n
+}
